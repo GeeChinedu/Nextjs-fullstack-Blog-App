e13c88909466e43efe4c6143e8efbeac0695c5ba
Gospel Ozorah <gospel.chinedu@gmail.com>
2024-04-12 21:42:27 +0100

    footer check

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
-import styles from './footer.module.css';
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>GospelDev</div>
-      <div className={styles.text}>Gospels creative thoughts agency © All rights reserved.</div>
+      <div className={styles.text}>
+        Gospels Inspired thoughts agency © All rights reserved.
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
